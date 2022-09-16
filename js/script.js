f29511e1e15a6bbe4130b03849a13390670287c7
Shermatov Davron Devloper full stack <93197110+DavronShermatov@users.noreply.github.com>
2022-09-16 09:54:55 +0300

    delegatsiya

--- js/script.js
+++ js/script.js
@@ -2,25 +2,25 @@
 
 const wrapper = document.querySelector('.btn-block')
 const btns = document.querySelectorAll('.button')
-// wrapper.addEventListener('click', (event)=>{
-//   if(event.target && event.target.tagName==='BUTTON'){
-//     console.log('clicked')
-//   }
-// })
-// wrapper.addEventListener('click', (event)=>{
-//   if(event.target && event.target.matches('button.blue')){
-//     console.log('clicked')
-//   }
-// })
-// wrapper.addEventListener('click', (event)=>{
-//   if(event.target && event.target.contains('blue')){
-//     console.log('clicked')
-//   }
-// })
+wrapper.addEventListener('click', (event)=>{
+  if(event.target && event.target.tagName==='BUTTON'){
+    console.log('clicked')
+  }
+})
+wrapper.addEventListener('click', (event)=>{
+  if(event.target && event.target.matches('button.blue')){
+    console.log('clicked')
+  }
+})
+wrapper.addEventListener('click', (event)=>{
+  if(event.target && event.target.contains('blue')){
+    console.log('clicked')
+  }
+})
 
-// const btn = document.createElement('button')
-// btn.classList.add('blue')
-// wrapper.append(btn)
+const btn = document.createElement('button')
+btn.classList.add('blue')
+wrapper.append(btn)
 
 
 btns.forEach((item) => {
