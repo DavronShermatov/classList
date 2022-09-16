4b3a07357921b03168d574b136d0e08ccf4f5d28
Shermatov Davron Devloper full stack <93197110+DavronShermatov@users.noreply.github.com>
2022-09-16 09:44:53 +0300

    gelegatsiya lesson

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 'use strict'
 
-const btns = document.querySelectorAll('button')
+const wrapper = document.querySelector('.btn-block')
+const btns = document.querySelectorAll('.button')
+// wrapper.addEventListener('click', (event)=>{
+//   if(event.target && event.target.tagName==='BUTTON'){
+//     console.log('clicked')
+//   }
+// })
+// wrapper.addEventListener('click', (event)=>{
+//   if(event.target && event.target.matches('button.blue')){
+//     console.log('clicked')
+//   }
+// })
+// wrapper.addEventListener('click', (event)=>{
+//   if(event.target && event.target.contains('blue')){
+//     console.log('clicked')
+//   }
+// })
 
-// classList.length bu belgilangan elementda nechta element bor ekanligini bildiradi
-console.log(btns[0].classList.length)
-// classList.item(1) bu belgilangan elementda classlari qanaqaligi haqida malumot beradi qavs ichidagi nechanchi element ekanligini bildiradi
-console.log(btns[0].classList.item(0))
-
-btns[1].classList.add('red')
-btns[0].classList.remove('red')
-
-// toggle bu tanlangan element classida blue bor bo'lsa olib tashlaydi yoq bo'lsa qo'shib qo'yadi yani teskarisini qiladi
-
-btns[2].classList.toggle('blue')
-
-// contains bu elementda tanlangan class bormi yoqmi shuni bizga aniqlab beradi
-if(btns[1].classList.contains('red')){
-  console.log('button  2-elementida red classi bor')
-}
+// const btn = document.createElement('button')
+// btn.classList.add('blue')
+// wrapper.append(btn)
 
 
-btns[0].addEventListener('click', ()=>{
-  if(!btns[1].classList.contains('red')){
-    btns[1].classList.add('red')
-  }else{
-    btns[1].classList.remove('red')
-  }
-  // btns[1].classList.toggle('red')
-})
+btns.forEach((item) => {
+  item.addEventListener('click', ()=>{
+      console.log('clicked')
+  })
+});
